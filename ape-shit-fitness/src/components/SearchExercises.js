d87f23e3f8be4b25e5b8055bf03c2883421f0a4a
Darryl Johnson <dandycodes@gmail.com>
2022-10-01 23:48:12 -0400

    Search Exercise Bar Created 10/1

--- ape-shit-fitness/src/components/SearchExercises.js
+++ ape-shit-fitness/src/components/SearchExercises.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, {useState, useEffect}  from 'react'
+import {Box, Button, Stack, TextField, Typography} from '@mui/material'
 
 const SearchExercises = () => {
-  return (
-    <div>SearchExercises</div>
-  )
+// Below will use the state to allow actions
+
+const [search, setSearch] = useState('')
+
+
+const handleSearch = async () => {
+    if(search) {
+        const exercisesData = await fetchData()
+    }
 }
+
+
+    return (
+        <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+          <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="49px" textAlign="center">
+            These Exercises Are <br /> For Your Inner Beast 
+          </Typography>
+          <Box position="relative" mb="72px">
+            <TextField
+              height="76px"
+              sx={{ input: { fontWeight: '700', border: 'none', borderRadius: '6px' }, width: { lg: '1170px', xs: '350px' }, backgroundColor: '#fff', borderRadius: '90px' }}
+              value={search}
+              onChange={(e) => setSearch(e.target.value.toLowerCase())}
+              placeholder="Browse Exercises!"
+              type="text"
+            />
+            <Button className="search-btn" sx={{ bgcolor: '#02295B', color: '#02295BA1', textTransform: 'none', width: { lg: '173px', xs: '80px' }, height: '56px', position: 'absolute', right: '0px', fontSize: { lg: '20px', xs: '14px' }, onClick:{handleSearch}}}>
+              Search
+            </Button>
+          </Box>
+          <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
+            {/* <HorizontalScrollbar data={bodyParts} bodyParts setBodyPart={setBodyPart} bodyPart={bodyPart} /> */}
+          </Box>
+        </Stack>
+      );
+    };
+    
 
 export default SearchExercises
