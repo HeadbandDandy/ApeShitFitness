9109b5c624c49557d1855450737f7b072bf71eaa
Darryl Johnson <dandycodes@gmail.com>
2022-10-09 10:57:33 -0400

    Detail components added

--- ape-shit-fitness/src/pages/ExerciseDetail.js
+++ ape-shit-fitness/src/pages/ExerciseDetail.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import {Box} from '@mui/material';
+
+//below imports utilities
+
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+
+
+//below imports Components needed for detail section
+import Detail from '../components/Detail';
+import ExerciseVideos from '../components/ExerciseVideos';
+import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams()
+
+// below recalls function everytime the ID changes
+useEffect(() => {
+  const fetchExercisesData = async () => {
+    const exerciseDBUrl = ''
+  }
+
+  fetchExercisesData();
+}, [id])
+
+
+
   return (
-    <div>ExerciseDetail</div>
+    <Box>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
   )
 }
 
