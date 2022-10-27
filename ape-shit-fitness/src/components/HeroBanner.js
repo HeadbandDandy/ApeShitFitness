af48a1c22a076963b745828b9202c46ab82e5d30
Darryl Johnson <dandycodes@gmail.com>
2022-10-27 19:40:52 -0400

    hero updated

--- ape-shit-fitness/src/components/HeroBanner.js
+++ ape-shit-fitness/src/components/HeroBanner.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
-import HeroBannerImage from '../assets/images/banner.png';
+import HeroBannerImage from '../assets/images/ape-png-1.png';
 
 const HeroBanner = () => (
   <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
-    <Typography color="#FF2625" fontWeight="600" fontSize="26px">Fitness Club</Typography>
+    <Typography color="gold" fontWeight="600" fontSize="80px">Welcome to  <br /> The Ape Club!</Typography>
     <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
-      Sweat, Smile <br />
-      And Repeat
+      Time to go <br />
+      Ape Shit!
     </Typography>
-    <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
-      Check out the most effective exercises personalized to you
+    <Typography fontSize="22px" fontFamily="Raleway" lineHeight="35px">
+      Click below to start personalizing your workouts!
     </Typography>
     <Stack>
-      <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#FF2625', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
+      <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: 'navy', padding: '14px', fontSize: '30px', textTransform: 'none', color: 'gold', borderRadius: '4px' }}>Find Your Lifts!</a>
     </Stack>
-    <Typography fontWeight={600} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
-      Exercise
+    <Typography fontWeight={600} color="#gold" sx={{ opacity: '0.2', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
+      Release <br/> The Beast!
     </Typography>
     <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
   </Box>
--- ape-shit-fitness/src/components/Navbar.js
+++ ape-shit-fitness/src/components/Navbar.js
@@ -2,22 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Stack } from '@mui/material';
 
-import Logo from '../assets/images/Logo.png';
+import Logo from '../assets/images/gorilla-clipart-strong-16.png';
 
 const Navbar = () => (
   <Stack direction="row" justifyContent="space-around" sx={{ gap: { sm: '123px', xs: '40px' }, mt: { sm: '32px', xs: '20px' }, justifyContent: 'none' }} px="20px">
     <Link to="/">
-      <img src={Logo} alt="logo" style={{ width: '48px', height: '48px', margin: '0px 20px' }} />
+      <img src={Logo} alt="logo" style={{ width: '100px', height: '100px', margin: '0px 10px' }} />
     </Link>
     <Stack
       direction="row"
       gap="40px"
-      fontFamily="Alegreya"
-      fontSize="24px"
-      alignItems="flex-end"
+      fontFamily="Raleway"
+      fontSize="40px"
+      alignItems="flex-start"
     >
-      <Link to="/" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>Home</Link>
-      <a href="#exercises" style={{ textDecoration: 'none', color: '#3A1212' }}>Exercises</a>
+      <Link to="/" style={{ textDecoration: 'none', color: 'gold', borderBottom: '3px solid navy' }}>Home</Link>
+      <a href="#exercises" style={{ textDecoration: 'none', color: 'gold' }}>Exercises</a>
     </Stack>
   </Stack>
 );
